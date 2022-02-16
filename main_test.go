5f15b892f2b310198b11a8c17d6c816b1f39ed42
Sayanta Banerjee <53281158+Sayanta66@users.noreply.github.com>
2022-02-16 14:51:52 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import "testing"
@@ -23,30 +22,4 @@ func Test_multiply(t *testing.T) {
 		t.Fatalf("wanted %d, got %d", want, got)
 	}
 
-=======
-package main
-
-import "testing"
-
-func Test_add(t *testing.T)  {
-	start, plus := 10, 10
-	want := start + plus
-	got := add(start, plus)
-
-	if want != got {
-		t.Fatalf("wanted %d, got %d", want, got)
-	}
-
-}
-
-func Test_multiply(t *testing.T) {
-	start, times:= 10, 10
-	want := start * times
-	got := multiply(start, times)
-
-	if want != got {
-		t.Fatalf("wanted %d, got %d", want, got)
-	}
-
->>>>>>> 38e00decfea55bb50fc074c81e078f8e0771f3a5
 }
