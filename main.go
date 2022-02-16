78b9c80ac8f254af0c34c7e41050f64eee2c8f83
Sayanta Banerjee <53281158+Sayanta66@users.noreply.github.com>
2022-02-16 14:48:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 func main() {
@@ -16,22 +15,4 @@ func multiply(n, multiplyBy int) int {
 	}
 	
 	return result
-=======
-package main
-
-func main() {
-	println(add(100,100))
-}
-
-func add(n, add int) int {
-	return n+add
-}
-
-func multiply(n, multiplyBy int) int {
-	result := n
-	for i := 1; i < multiplyBy; i++ {
-		result = add(result, n)
-	}
-	return result
->>>>>>> 38e00decfea55bb50fc074c81e078f8e0771f3a5
-}
+	
